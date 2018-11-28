035a188ad30fe547970c05c271d61f2f93328b6f
Emanuele <emanuelegorga@gmail.com>
2018-11-28 15:57:21 +0000

    Docking station releasing a working bike

--- spec/borisbikes_spec.rb
+++ spec/borisbikes_spec.rb
@@ -1,6 +1,14 @@
 require 'borrisbikes'
+
 describe DockingStation do
+
    it { is_expected.to respond_to(:release_bike)}
+
+   it 'release a working bike' do
+     docking_station = DockingStation.new
+     expect(docking_station.release_bike.working?).to eq true
+   end
+
 end
 
 #docking_station = DockingStation.new
