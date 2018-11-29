cc79878c1414140f48e5ca1bc5f9423f1b299038
Joao Abbott-Gribben <joao.abbott.gribben@gmail.com>
2018-11-29 16:16:15 +0000

    updated rspec to test that DockingStation class can receive, store and retrieve instances of the Bike class

--- lib/borrisbikes.rb
+++ lib/borrisbikes.rb
@@ -2,11 +2,17 @@ require_relative 'bike'
 
 class DockingStation
 attr_reader :dock
+
   def release_bike
     Bike.new
   end
 
   def dock(bike)
-    Bike.new
+    @bike = bike
   end
+
+  def bike
+  	@bike 
+  end
+
 end
--- spec/borisbikes_spec.rb
+++ spec/borisbikes_spec.rb
@@ -12,9 +12,21 @@ describe DockingStation do
  #Putting different bikes in docking station
 
    it 'will dock a bike (responding to the dock method)' do
-     bike = DockingStation.new
-     expect(bike).to respond_to(:dock).with(1).argument
+     docking_station = DockingStation.new
+     expect(docking_station).to respond_to(:dock).with(1).argument
    end
+
+  it 'docks something' do
+    bike = Bike.new
+    expect(subject.dock(bike)).to eq bike
+    end
+  end
+
+  it 'returns docked bikes' do
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.bike).to eq bike
+  end
 
 end
 
